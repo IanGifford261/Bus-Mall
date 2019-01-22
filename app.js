532d3584b8a3be24cd10ab7f6a99e74fee52654c
iangifford <lightyagamie10@gmail.com>
2019-01-21 18:24:02 -0800

    adds more css, images, and some more framework

--- app.js
+++ app.js
@@ -3,28 +3,51 @@
 var allPics = [];
 var allMall= document.getElementById('allMall');
 
-function mallItems(what){
+var imgOne = document.getElementById('imgOne');
+var imgTwo = document.getElementById('imgTwo');
+var imgThree = document.getElementById('imgThree');
+
+
+function MallItems(what){
   this.filepath = `images/${what}.jpg`;
   this.what = what;
   this.views = 0;
   allPics.push(this);
 }
 
-new MallPic('bag');
-new MallPic('banana');
-new MallPic('bathroom');
-new MallPic('boots');
-new MallPic('breakfast');
-new MallPic('bubblegum');
-new MallPic('chair');
-new MallPic('cthulhu');
-new MallPic('dog-duck');
-new MallPic('dragon');
-new MallPic('pen');
-new MallPic('pet-sweep');
-new MallPic('scissors');
-new MallPic('tauntaun');
-new MallPic('unicorn');
-new MallPic('usb');
-new MallPic('water-can');
-new MallPic('wine-glass');
+new MallItems('bag');
+new MallItems('banana');
+new MallItems('bathroom');
+new MallItems('boots');
+new MallItems('breakfast');
+new MallItems('bubblegum');
+new MallItems('chair');
+new MallItems('cthulhu');
+new MallItems('dog-duck');
+new MallItems('dragon');
+new MallItems('pen');
+new MallItems('pet-sweep');
+new MallItems('scissors');
+new MallItems('tauntaun');
+new MallItems('unicorn');
+new MallItems('shark');
+new MallItems('sweep');
+new MallItems('usb');
+new MallItems('water-can');
+new MallItems('wine-glass');
+
+function randItem(){
+  var random = Math.floor(Math.random() *allPics.length);
+  imgOne.src = allPics[0].filepath;
+  imgOne.alt = allPics[0].what;
+  imgOne.title = allPics[0].what;
+  imgTwo.src = allPics[1].filepath;
+  imgTwo.alt = allPics[1].what;
+  imgTwo.title = allPics[1].what;
+  imgThree.src = allPics[2].filepath;
+  imgThree.alt = allPics[2].what;
+  imgThree.title = allPics[2].what;
+  allPics[random].views++;
+}
+
+randItem();
